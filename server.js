415fdfb93a70741cd060d81bf6d24c1051b4134b
Danny Carcamo <dannycarcamo@icloud.com>
2021-01-22 12:48:23 -0600

    updates before deploy

--- server.js
+++ server.js
@@ -10,7 +10,13 @@ const MOVIEDEX = require('./movies-data-small.json');
 
 const app = express();
 
-app.use(morgan('dev'));
+const morganOption = (process.env.NODE_ENV === 'production')
+  ? 'tiny'
+  : 'common';
+
+  
+app.use(morgan(morganOption));
+
 app.use(helmet());
 app.use(cors());
 app.use(function validateBearerToken(req, res, next) {
@@ -50,7 +56,17 @@ function handleGetMovies(req, res) {
 
 app.get('/movie', handleGetMovies);
 
-const PORT = 8000;
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }};
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
+});
+
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`);
